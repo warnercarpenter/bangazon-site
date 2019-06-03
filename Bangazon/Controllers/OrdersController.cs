4eb68533261ca8132fe6dd5b8559a9cea4da8746
Warner Carpenter <warnercarpenter@yahoo.com>
2019-06-03 10:17:39 -0500

    starting to mess with cart

--- Bangazon/Controllers/OrdersController.cs
+++ Bangazon/Controllers/OrdersController.cs
@@ -22,6 +22,7 @@ namespace Bangazon.Controllers
         // GET: Orders
         public async Task<IActionResult> Index()
         {
+
             var applicationDbContext = _context.Order.Include(o => o.PaymentType).Include(o => o.User);
             return View(await applicationDbContext.ToListAsync());
         }
