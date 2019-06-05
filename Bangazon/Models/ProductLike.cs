eef66eeca2c4333fc24841fc6a223247af07c740
Warner Carpenter <warnercarpenter@yahoo.com>
2019-06-05 10:47:20 -0500

    added models for ProductLike and ProductRating

---
+++ Bangazon/Models/ProductLike.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web;
+
+namespace Bangazon.Models
+{
+    public class ProductLike
+    {
+        [Key]
+        public int ProductLikeId { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public string ProductId { get; set; }
+
+        public ApplicationUser User { get; set; }
+        public Product Product { get; set; }
+
+        [Required]
+        public bool IsLiked { get; set; }
+    }
+}
---
+++ Bangazon/Models/Rating.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web;
+
+namespace Bangazon.Models
+{
+    public class ProductRating
+    {
+        [Key]
+        public int ProductRatingId { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public string ProductId { get; set; }
+
+        public ApplicationUser User { get; set; }
+        public Product Product { get; set; }
+
+        [Required(ErrorMessage = "Please select a rating")]
+        [Display(Name = "Rating")]
+        [Range(1, 5)]
+        public int Rating { get; set; }
+    }
+}
